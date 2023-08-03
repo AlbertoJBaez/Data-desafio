5943ab645982f800f801f466b7ac96152293a323
albertojimenez <jimenezbaezalberto@gmail.com>
2023-08-03 12:08:50 +0200

    actualizacion depo y deploy

--- Chat_bot/app.py
+++ Chat_bot/app.py
@@ -62,6 +62,7 @@ def chatbot_client():
         return f"Error: {str(e)}"    
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False),
+    
 
  
